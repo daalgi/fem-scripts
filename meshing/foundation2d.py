63eebecab24347a6400daea5c9c4b4e0ebfcd8f3
daalgi <daalgi.cam@gmail.com>
2020-10-08 23:33:05 +0200

    update

--- meshing/foundation2d.py
+++ meshing/foundation2d.py
@@ -13,34 +13,51 @@ div_in = 5
 div_load = 2
 div_out = 3
 div_slab = 10
-div_circ = 12
+div_circ = 24
 divs = [div_in, div_load, div_out, div_slab]
 
 with pygmsh.geo.Geometry() as geom:   
     geom.characteristic_length_max = 0.5
 
     pointc = geom.add_point((0, 0, 0), mesh_size=1)
-    pointsh = [pointc] + [geom.add_point((r, 0, 0), mesh_size=1) for r in radius]
-    pointsv = [pointc] + [geom.add_point((0, r, 0), mesh_size=1) for r in radius]
+    
+    pointsh = [geom.add_point((-r, 0, 0), mesh_size=1) for r in radius[::-1]]
+    pointsh += [pointc] + [geom.add_point((r, 0, 0), mesh_size=1) for r in radius]
 
-    arcs = [geom.add_circle_arc(p, pointc, q) for p, q in zip(pointsh[1:], pointsv[1:])]
+    pointsv = [geom.add_point((0, -r, 0), mesh_size=1) for r in radius[::-1]]
+    pointsv += [pointc] + [geom.add_point((0, r, 0), mesh_size=1) for r in radius]
+
+    #arcs = [geom.add_circle_arc(p, pointc, q) for p, q in zip(pointsh[1:], pointsv[1:])]
+    quadrant_divs = len(radius)
+    # Arcs for each quadrant from the center outwards
+    arcs_q1 = [geom.add_circle_arc(p, pointc, q) for p, q in zip(pointsh[quadrant_divs+1:], pointsv[quadrant_divs+1:])]
+    arcs_q2 = [geom.add_circle_arc(p, pointc, q) for p, q in zip(pointsv[quadrant_divs+1:], pointsh[:quadrant_divs][::-1])]
+    arcs_q3 = [geom.add_circle_arc(p, pointc, q) for p, q in zip(pointsh[:quadrant_divs][::-1], pointsv[:quadrant_divs][::-1])]
+    arcs_q4 = [geom.add_circle_arc(p, pointc, q) for p, q in zip(pointsv[:quadrant_divs][::-1], pointsh[quadrant_divs+1:])]
+    arcs = arcs_q1 + arcs_q2 + arcs_q3 + arcs_q4
+    
     lh = [geom.add_line(p, q) for p, q in zip(pointsh[:-1], pointsh[1:])]
     lv = [geom.add_line(p, q) for p, q in zip(pointsv[:-1], pointsv[1:])]    
-
-    cloops = [geom.add_curve_loop([lh[0], arcs[0], -lv[0]])]
-    cloops += [geom.add_curve_loop([h, cext, -v, -cint]) for h, cext, v, cint in zip(lh[1:], arcs[1:], lv[1:], arcs[:-1])]
+    
+    #cloops = [geom.add_curve_loop([lh[0], arcs[0], -lv[0]])]
+    #cloops += [geom.add_curve_loop([h, cext, -v, -cint]) for h, cext, v, cint in zip(lh[1:], arcs[1:], lv[1:], arcs[:-1])]
+    #surfaces = [geom.add_surface(cloop) for cloop in cloops]
+    #cloops = [geom.add_curve_loop([a, b, c, d]) for a, b, c, d in zip(arcs_q1, arcs_q2, arcs_q3, arcs_q4)]
+    cloops = [geom.add_curve_loop([arcs_q1[0], arcs_q2[0], arcs_q3[0], arcs_q4[0]])]
+        
     surfaces = [geom.add_surface(cloop) for cloop in cloops]
+
 
     for a in arcs:
         geom.set_transfinite_curve(a, div_circ, "Progression", 1)
-
+    """
     for h, v, div in zip(lh, lv, divs):
         geom.set_transfinite_curve(h, div, "Progression", 1)
         geom.set_transfinite_curve(v, div, "Progression", 1)
 
     for s in surfaces[1:]:
-        geom.set_transfinite_surface(s, corner_tags=s.curve_loop.curves)
-    
+        geom.set_transfinite_surface(s, "Left", [])
+    """
     #geom.set_recombined_surfaces(surfaces)
     
     mesh = geom.generate_mesh()
